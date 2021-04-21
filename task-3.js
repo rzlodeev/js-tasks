d4d5243c25e17f01bf0cfb851d71d8bc0addd3e7
rzlodeev <81644015+rzlodeev@users.noreply.github.com>
2021-04-21 17:19:43 +0300

    Update task-3.js

--- task-3.js
+++ task-3.js
@@ -1,13 +1,10 @@
 
 //https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
-//не працює, а чому?
-
-
 function giveMeFive(obj) {
-  let arr = [];
+  const arr = [];
   for (let key in obj) {
     if (key.length === 5) {arr.push(key);}
     if (obj[key].length === 5) {arr.push(obj[key]);}
   }
-  
+  return(arr);
 }
